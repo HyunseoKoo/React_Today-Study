26d2fbccf5ced2171429edb5b067c0ff597cc3b7
HyunseoKoo <koohs414@naver.com>
2023-03-17 23:36:28 +0900

    fixed: openApi_RTK 폴더 수정
    페이지네이션 기능 구현 시작합니다.

--- openApi_RTK/src/pages/mainpage/index.js
+++ openApi_RTK/src/pages/mainpage/index.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import IssueList from './components/fetchIssues/issueList';
 
 function MainPage() {
+  // 필요한 변수 (1.한 페이지에 나올 이슈카드 수 2.페이지 수 3.현재 인덱스 4.)
+  const [perPage, setPerPage] = useState(10);
+  const [offset, setOffset] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState();
+
+  // const [perPage, setPerPage] = useState(10);
+  // const [offset, setOffset] = useState(1);
+  // const [currentIndex, setCurrentIndex] = useState(0);
+  // const [pageOffset, setPageOffset] = useState();
+
+  const issues = useSelector((state) => state.issue.issues);
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getIssues({ perPage, offset }));
+  //   const totalPageNum
+  //   pageNum
+  //   pageNum
+  //   for (let i = 0; i < totalPageNum; i += 10) {
+  //     pageArr.push();
+  //   }
+  //   setPageOffset(pageArr[currentIndex]);
+  // }, [perPage, offset, currentIndex]);
+
   return (
     <>
       <div>Git Issue.net</div>
