2451ebe34f7bb9169dd1592f352e02301b3e106a
hyunseoKoo <koohs414@naver.com>
2023-05-17 13:41:30 +0900

    create: 반응형 사이트 구현을 위한 react-responsive 라이브러리 연습

---
+++ react_responsive/src/App.js
@@ -0,0 +1,25 @@
+import {useMediaQuery} from 'react-responsive'
+
+function App() {
+  const isDesktopOrMobile = useMediaQuery({query: '(max-width: 768px'});
+
+  return (
+    <div className="App">
+
+      {isDesktopOrMobile !== true ?
+        <div style={{border: "1px solid #dbdbdb", width: "1000px"}}>
+          <h1>웹 화면입니다.</h1>
+          <h2>줄어들기 전 입니다.</h2>
+        </div> 
+        :
+        <div style={{border: "1px solid #d9d9d9", width: "500px"}}>
+          <h1>모바일 화면입니다:)</h1>
+          <h2>줄어든 후 입니다.</h2>
+        </div>  
+      }
+
+    </div>
+  );
+}
+
+export default App;
